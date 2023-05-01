45422b3b4ff545a5d355a5a0fa4cb2a082ae1558
rsa16 <eludeservices@proton.me>
2023-05-01 19:49:52 -0400

    added nice background color to boddy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,16 @@ export default defineNuxtConfig({
     modules: ['@vueuse/motion/nuxt'],
     css: [
         '@fortawesome/fontawesome-svg-core/styles.css'
-    ]
+    ],
+    app: {
+        head: {
+            bodyAttrs: {
+                style: "background-image: linear-gradient(106.83deg, \
+                    rgb(82, 0, 255) -1.36%, \
+                    rgb(159, 0, 151) 36.23%, \
+                    rgb(188, 0, 87) 64.28%, \
+                    rgb(255, 0, 0) 106.35%); background-color: initial; margin: 0;"
+            }
+        }
+    }
 })
